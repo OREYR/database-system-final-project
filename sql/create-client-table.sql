885f8493f4a1b596cd04e59ca259771774876e0e
CHERYL FAN <63218145+OREYR@users.noreply.github.com>
2022-12-22 00:34:58 -0500

    Update create-client-table.sql

--- sql/create-client-table.sql
+++ sql/create-client-table.sql
@@ -1,15 +1,40 @@
-CREATE DATABASE 'customer;
-USE customer;
+create database insurance_claim;
+
+use insurance_claim;
 
 create table client (
-	id int(5) NOT NULL AUTO_INCREMENT,
-    lastName varchar(100) NOT NULL,
-    firstName varchar(100) NOT NULL,
+    id int(5) not null auto_increment,
+    lastName varchar(100) not null,
+    firstName varchar(100) not null,
     middleInitial varchar(10),
-    birthDate  ,
+    birthDate varchar(50),
+    age int(50),
     gender varchar(3),
     ssn varchar(11),
     mailAddress varchar(200),
-    PRIMARY KEY (id)
+    fraudRate decimal(4,3),
+    primary key (id)
 );
+
+create table claim_client_relation (
+    id int(5) not null auto_increment,
+    clientId int(5),
+    age int(50),
+    race varchar(20),
+    state varchar(20),
+    county varchar(20),
+    diseaseCode int(500),
+    claimStartDt varchar(50),
+    claimEndDt varchar(50),
+    provider varchar(100),
+    annualReimbursementAmt double,
+    annualDeductibleAmt double,
+    potentialFraudRate decimal(4,3),
+    primary key (id),
+    constraint fk_category
+    foreign key (clientID) 
+        references client(id)
+);
+    
+
 
